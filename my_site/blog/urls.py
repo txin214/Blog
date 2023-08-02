bf03ac4eca28af2a9b1826cc83ccf3e0e777b272
1090062661 <1090062661@qq.com>
2023-08-02 01:32:01 -0500

    Add models and migrations

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="post-page"),
+    path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, 
          name="post-detail-page")
 ]
