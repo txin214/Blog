76df0b617e2b491e9ee6d901b601d7684af7d28e
1090062661 <1090062661@qq.com>
2023-08-02 11:17:37 -0500

    Modifying the views and urls

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -22,7 +22,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index = True)
     content = models.TextField(validators = [MinLengthValidator(10)])
@@ -32,3 +32,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_emali = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -123,8 +123,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
